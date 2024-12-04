b4ca2f239d7a810fd9034267a5f069b90d16315c
sudedaka <sude__daka@hotmail.com>
2024-12-04 22:44:51 +0300

    fxml testing

--- src/main/java/com/example/timetablemanager/TimetableManager.java
+++ src/main/java/com/example/timetablemanager/TimetableManager.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TimetableManager extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TimetableManager.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TimetableManager.class.getResource("deneme.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/timetablemanager/denemeController.java
@@ -0,0 +1,11 @@
+package com.example.timetablemanager;
+
+import javafx.scene.control.Button;
+
+public class denemeController {
+
+    Button newButton = new Button("deneme");
+
+
+
+}
