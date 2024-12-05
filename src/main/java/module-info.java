9143c5e06e6ec4468c5511fd472db5d2b9efe812
Alp KOÇAK <alpqkocak@gmail.com>
2024-12-05 21:33:20 +0300

    Maven Test
    Test for maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module com.example.timetablemanager {
+module com.example.macven {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens com.example.timetablemanager to javafx.fxml;
-    exports com.example.timetablemanager;
+    opens com.example.macven to javafx.fxml;
+    exports com.example.macven;
 }
